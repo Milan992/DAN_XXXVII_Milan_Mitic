b2cb8cda2790f48444715734552e70a9ea8e4023
Milan992 <mitic.gsm@gmail.com>
2020-06-23 14:17:02 +0200

    possibly the program is running correctly

--- DAN_XXXVII_Milan_Mitic/TruckApp/Program.cs
+++ DAN_XXXVII_Milan_Mitic/TruckApp/Program.cs
@@ -15,10 +15,16 @@ namespace TruckApp
         static List<int> divisable = new List<int>();
         static int[] chosenRoutes = new int[10];
         static Random random = new Random();
+        static Thread[] trucks = new Thread[10];
+        static Semaphore semaphore = new Semaphore(2, 2);
+        static int routTime;
+        static int i = -1;
 
         static void Main(string[] args)
         {
+            // clean txt file
             File.WriteAllText(@"..\..\possibleRoutes.txt", "");
+
             Thread begin = new Thread(() => GenerateRoutNumber());
             Thread manager = new Thread(() => ChooseRoute());
 
@@ -26,16 +32,22 @@ namespace TruckApp
             manager.Start();
             manager.Join();
 
+            Thread t = new Thread(() => FillTruck());
             for (int i = 0; i < 10; i++)
             {
-                Thread t = new Thread(() => FillTruck());
+                t = new Thread(() => FillTruck());
                 t.Name = "Truck_" + Convert.ToString(i + 1);
+                trucks[i] = t;
                 t.Start();
             }
-            
+            t.Join();
+
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// generates 1000 random numbers and writes them to a txt file.
+        /// </summary>
         public static void GenerateRoutNumber()
         {
             using (StreamWriter sw = new StreamWriter(@"..\..\possibleRoutes.txt", true))
@@ -57,7 +69,10 @@ namespace TruckApp
             Console.WriteLine("\nRout numbers generated");
         }
 
-        private static void ChooseRoute()
+        /// <summary>
+        /// puts all the numbers divisable by 3 in a list.
+        /// </summary>
+        public static void ChooseRoute()
         {
             Thread.Sleep(random.Next(1, 3001));
             Console.WriteLine("\nManager notifies drivers that routes are chosen");
@@ -86,11 +101,40 @@ namespace TruckApp
             }
         }
 
-        private static void FillTruck()
+        /// <summary>
+        /// simulates time needed to fill a truck.
+        /// </summary>
+        public static void FillTruck()
         {
+            semaphore.WaitOne();
             int timeFilling = random.Next(500, 5001);
             Thread.Sleep(timeFilling);
             Console.WriteLine("\n{0} was filled in {1} miliseconds", Thread.CurrentThread.Name, timeFilling);
+            semaphore.Release();
+            i++;
+            Console.WriteLine("\n{0} gets the rout {1}", Thread.CurrentThread.Name, chosenRoutes[i]);
+            routTime = random.Next(500, 5001);
+            Thread order = new Thread(() => Rout(routTime, timeFilling));
+            order.Name = Thread.CurrentThread.Name;
+            order.Start();
+            Console.WriteLine("\n{0} notifies the order that he will arrive in {1} miliseconds", Thread.CurrentThread.Name, routTime);
+        }
+
+        public static void Rout(int routTime, int timeFilling)
+        {
+            if (routTime < 3000)
+            {
+                Thread.Sleep(routTime);
+                Console.WriteLine("\n{0} arrived", Thread.CurrentThread.Name);
+                double time = timeFilling / 1.5;
+                Thread.Sleep(Convert.ToInt32(time));
+                Console.WriteLine("\n{0} unloaded in {1} miliseconds", Thread.CurrentThread.Name, Convert.ToInt32(time));
+            }
+            else
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("\nOrder canceled, {0} came back in 3 seconds", Thread.CurrentThread.Name);
+            }
         }
     }
 }
